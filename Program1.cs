3563ec5dc7e6367c6395bb2bf07b517a248cbb23
ctdung18i1 <48077503+ctdung18i1@users.noreply.github.com>
2019-02-28 10:46:42 +0700

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baikiemtra2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tinhtong tt = new tinhtong();
+            tt.tinh();
+            tt.In();
+            Console.ReadLine();
+        }
+    }
+}
