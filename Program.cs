a1fe8e71fcb1df00755b9b79aa8ca365b55985c5
ctdung18i1 <48077503+ctdung18i1@users.noreply.github.com>
2019-03-22 12:26:30 +0700

    Add files via upload
    Em nộp bài tập C# ngày 21/03/2019

--- Program.cs
+++ Program.cs
@@ -4,14 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace baikiemtra
+namespace Sinhvien
 {
     class Program
     {
         static void Main(string[] args)
         {
-            doantau tong = new doantau();
-            tong.tinh();
+            int n;
+            Console.WriteLine("Nhap so sinh vien");
+            n = Int32.Parse(Console.ReadLine());
+            // tao mang doi tuong sinh vien
+            SinhVien[] sv = new SinhVien[n];
+            for (int i = 0; i < n; i++)
+            {
+                sv[i] = new SinhVien();
+                sv[i].nhap();
+                sv[i].In();
+            }
+            float min = sv[0].tinhtb();
+            for (int i = 1; i < n; i++)
+            {
+                if (sv[i].tinhtb() < min) min = sv[i].tinhtb();
+            }
+            Console.WriteLine(" Sinh vien co diem thap nhat la: {0}", min);
+            for (int i = 1; i < n; i++)
+            {
+                if (sv[i].MaSV.Contains("IT"))
+                    Console.WriteLine(" SV ma {0} co diem tb la {1}", sv[i].MaSV, sv[i].tinhtb());
+            }
+            // sap xep mang sv theo diem tb tang giam dan?
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (sv[j].tinhtb() < sv[i].tinhtb())
+                    {
+                        SinhVien t = new SinhVien();
+                        t = sv[i];
+                        sv[i] = sv[j];
+                        sv[j] = t;
+                    }
+                }
+            }
+            Console.WriteLine("Diem Trung Binh Sap Xep Theo Thu Tu Tang Dan:");
+            for (int i = 0; i < n; i++)
+                Console.WriteLine("MaSv: {0} Diem TB: {1}", sv[i].MaSV, sv[i].tinhtb());
             Console.ReadLine();
         }
     }
---
+++ SinhVien.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sinhvien
+{
+    class SinhVien
+    {
+       public String MaSV;
+        float Toan;
+        float Ly;
+        float Hoa;
+        // default constructors
+        public SinhVien()
+        {
+            this.MaSV = "01";
+            this.Toan = (float)7.5;
+            this.Ly = (float)8.5;
+            this.Hoa = (float)6.0;
+        }
+        // tham so hoa constructors
+        public SinhVien(String MaSv, float Toan, float Ly, float Hoa)
+        {
+            this.MaSV = MaSV;
+            this.Toan = Toan;
+            this.Ly = Ly;
+            this.Hoa = Hoa;
+        }
+        // viet ham nhap diem
+        // viet ham tinh diem trung binh
+        // viet ham in
+        public void nhap()
+        {
+            Console.WriteLine("Nhap ma");
+            this.MaSV = Console.ReadLine();
+            Console.WriteLine("Nhap diem toan");
+            this.Toan = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap diem Ly");
+            this.Ly = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap diem Hoa");
+            this.Hoa = float.Parse(Console.ReadLine());
+        }
+        public float tinhtb()
+        {
+            return ((this.Toan + this.Ly + this.Hoa) / 3);
+        }
+        public void In()
+        {
+            Console.WriteLine("Ma sinh vien: {0} co diem trung binh la {1}", this.MaSV, this.tinhtb());
+
+        }
+    }
+}
